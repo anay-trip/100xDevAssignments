e4c27be01abb0dfd98fafa7997f0bd9a62f44022
Anay Trip <anaytripathi1@gmail.com>
2024-08-16 05:10:49 +0530

    all test cases passing for week-2-js/easy

--- week-2/week-2-js/easy/expenditure-analysis.js
+++ week-2/week-2-js/easy/expenditure-analysis.js
@@ -3,18 +3,43 @@
   and return a list of objects where each object is unique category-wise and has total price spent as its value.
   transactions is an array where each
   Transaction - an object like 
-        {
-		id: 1,
-		timestamp: 1656076800000,
-		price: 10,
-		category: 'Food',
-		itemName: 'Pizza',
-	}
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  }
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+
+  // Create a map with category as key and total spent as value
+  let map = new Map();
+  for (let transaction of transactions) {
+    if (map.has(transaction.category)) {
+      map.set(transaction.category, map.get(transaction.category) + transaction.price);
+    } else {
+      map.set(transaction.category, transaction.price);
+    }
+  }
+
+  let result = [];
+  for (let [key, value] of map.entries()) {
+    result.push({ category: key, totalSpent: value });
+  }
+  return result;
 }
+
+// Test Cases
+console.log(calculateTotalSpentByCategory([
+  { id: 1, timestamp: 1656076800000, price: 10, category: 'Food', itemName: 'Pizza' },
+  { id: 2, timestamp: 1656076800000, price: 20, category: 'Food', itemName: 'Pizza' },
+  { id: 3, timestamp: 1656076800000, price: 30, category: 'Food', itemName: 'Pizza' },
+  { id: 4, timestamp: 1656076800000, price: 40, category: 'Food', itemName: 'Pizza' },
+  { id: 5, timestamp: 1656076800000, price: 50, category: 'Food', itemName: 'Pizza' },
+  { id: 6, timestamp: 1656076800000, price: 6000, category: 'Vehicle', itemName: 'bike' },
+]));
 
 module.exports = calculateTotalSpentByCategory;
--- week-2/week-2-js/easy/findLargestElement.js
+++ week-2/week-2-js/easy/findLargestElement.js
@@ -1,12 +1,28 @@
 /*
-  Write a function `findLargestElement` that takes an array of numbers and returns the largest element.
-  Example:
-  - Input: [3, 7, 2, 9, 1]
-  - Output: 9
+  Write a function `findLargestElement` that takes an array of numbers and returns the largest element.
+  Example:
+  - Input: [3, 7, 2, 9, 1]
+  - Output: 9
 */
 
 function findLargestElement(numbers) {
-    
+
+    // if the array is empty return undefined  
+    // if (numbers.length === 0) {
+    //     return undefined;
+    // }
+    // let largest = numbers[0];
+    // for (let i = 0; i < numbers.length; i++) {
+    //     if (numbers[i] > largest) {
+    //         largest = numbers[i];
+    //     }
+    // }
+    // return largest;
+
+    if (numbers.length === 0) {
+        return undefined;
+    }
+    return numbers.reduce((a, b) => (a > b ? a : b));
 }
 
 module.exports = findLargestElement;
